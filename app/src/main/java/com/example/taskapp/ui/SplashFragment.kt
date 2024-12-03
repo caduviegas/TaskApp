470aac2a0852e8892428f0b92657c9ad2ae5b34b
Eduardo Viegas <eduardo.viegas@mosaico.com.br>
2024-12-03 15:43:35 -0300

    creation of navigation screens

--- app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.example.taskapp.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentSplashBinding
 
@@ -23,5 +26,18 @@ class SplashFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 10000)
+        checkAuth()
+    }
+
+    private fun checkAuth() {
+        if (isAdded) {
+            findNavController().navigate(R.id.action_splashFragment_to_authentication)
+        }
+
     }
 }
