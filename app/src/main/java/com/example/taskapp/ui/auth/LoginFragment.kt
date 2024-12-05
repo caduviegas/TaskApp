90cefe0cabc84df87c25316216ae14f4968d198b
Eduardo Viegas <eduardo.viegas@mosaico.com.br>
2024-12-05 20:55:19 -0300

    creation and configuration of tabLayout

--- app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
@@ -28,6 +28,9 @@ class LoginFragment : Fragment() {
     }
 
     private fun initListerners() {
+        binding.btnLogin.setOnClickListener{
+            findNavController().navigate(R.id.action_global_homeFragment)
+        }
         binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
