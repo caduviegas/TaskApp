fb34ac903b7209820a7ab6eb161105d8552dda25
Eduardo Viegas <eduardo.viegas@mosaico.com.br>
2024-12-16 19:15:45 -0300

    config indicators of tasks

--- app/src/main/java/com/example/taskapp/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/taskapp/ui/adapter/TaskAdapter.kt
@@ -1,12 +1,19 @@
 package com.example.taskapp.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.example.taskapp.R
+import com.example.taskapp.data.model.Status
 import com.example.taskapp.data.model.Task
 import com.example.taskapp.databinding.ItemTaskBinding
 
 class TaskAdapter(
+    private val context: Context,
     private val taskList: List<Task>
 ) : RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
 
@@ -24,9 +31,38 @@ class TaskAdapter(
     override fun getItemCount(): Int = taskList.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-       val task = taskList[position]
+        val task = taskList[position]
 
         holder.binding.textDescription.text = task.description
+        setIndicators(task, holder)
+    }
+
+    private fun setIndicators(task: Task, holder: MyViewHolder) {
+        when (task.status) {
+            Status.TODO -> {
+                holder.binding.btnBack.isVisible = false
+            }
+
+            Status.DOING -> {
+                holder.binding.btnBack.setColorFilter(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.color_status_todo
+                    )
+                )
+                holder.binding.btnNext.setColorFilter(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.color_status_done
+                    )
+                )
+            }
+
+            Status.DONE -> {
+                holder.binding.btnNext.isVisible = false
+            }
+        }
+
     }
 
     inner class MyViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
