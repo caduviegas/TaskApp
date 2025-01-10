0aea562309772167b3074b9db37299d7dc4d8622
Eduardo Viegas <eduardo.viegas@mosaico.com.br>
2025-01-10 18:24:52 -0300

    move new Tasks for MVVM

--- app/src/main/java/com/example/taskapp/ui/TodoFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/TodoFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.taskapp.R
 import com.example.taskapp.data.model.Status
 import com.example.taskapp.data.model.Task
@@ -65,6 +66,23 @@ class TodoFragment : Fragment() {
     }
 
     private fun observeViewModel() {
+        viewModel.taskInsert.observe(viewLifecycleOwner) { task ->
+            if (task.status == Status.TODO) {
+                //Armazena a lista atual do adapter
+                val oldList = taskAdapter.currentList
+
+                //Gera uma nova lista a partir da lista antiga já com a tarefa atualizada
+                val newList = oldList.toMutableList().apply {
+                    add(0, task)
+                }
+
+                //Envia a lista atualizada para o Adapter
+                taskAdapter.submitList(newList)
+
+                setPositionRecyclerView()
+
+            }
+        }
         viewModel.taskUpdate.observe(viewLifecycleOwner) { updateTask ->
             if (updateTask.status == Status.TODO) {
 
@@ -139,7 +157,7 @@ class TodoFragment : Fragment() {
         FirebaseHelper.getDatabase()
             .child("tasks")
             .child(FirebaseHelper.getIdUser())
-            .addValueEventListener(object : ValueEventListener {
+            .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     val taskList = mutableListOf<Task>()
                     for (ds in snapshot.children) {
@@ -156,10 +174,33 @@ class TodoFragment : Fragment() {
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-                    Log.i("INFOTESTE","onCancelled:")
+                    Log.i("INFOTESTE", "onCancelled:")
                 }
 
             })
+    }
+
+    private fun setPositionRecyclerView() {
+        taskAdapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onChanged() {
+            }
+
+            override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+            }
+
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                binding.rvTasks.scrollToPosition(0)
+            }
+
+            override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+            }
+
+            override fun onItemRangeChanged(positionStart: Int, itemCount: Int, payload: Any?) {
+            }
+
+            override fun onItemRangeMoved(fromPosition: Int, toPosition: Int, itemCount: Int) {
+            }
+        })
     }
 
     private fun deleteTasks(task: Task) {
